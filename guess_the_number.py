d0dddf6bc8ee888d5e21130ae2f92e06dd92fa44
Gourab <gourabgaming111@gmail.com>
2023-10-25 23:57:41 +0530

    Checking without loop the user given number to the randomly generated number

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,8 +1,14 @@
 # print("Hello World!")
 import random
+secret = random.randint(1,10);
 
 user_input = int(input("Enter a number = "))
-print(user_input);
 
-secret = random.randint(1,10);
-print(secret);
+if user_input == secret:
+    print("Correct Guess!")
+elif user_input > secret:
+    print("Guess is high!")
+elif user_input < 0 and user_input > 10:
+    print("Guess out of bounds")
+else:
+    print("Guess is low")
