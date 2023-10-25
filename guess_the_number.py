5eb4754b507d2c8cb3e63e35ca89c9e80ac1256c
Gourab <gourabgaming111@gmail.com>
2023-10-26 00:14:51 +0530

    Added score and various comments to help understand

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,14 +1,28 @@
-# print("Hello World!")
 import random
 secret = random.randint(1,10);
 
-user_input = int(input("Enter a number = "))
+x = True
+score = 10
 
-if user_input == secret:
-    print("Correct Guess!")
-elif user_input > secret:
-    print("Guess is high!")
-elif user_input < 0 and user_input > 10:
-    print("Guess out of bounds")
-else:
-    print("Guess is low")
+# Statement
+print("Guess a number between 1 and 10, you have 10 tries, the higher the score the better.")
+
+while x:
+    user_input = int(input("Enter a number = "))    # User Input
+    if ((user_input) < 1 or (user_input) > 10):     # If the guess is out of bounds
+        print("Guess out of bounds")
+    elif user_input == secret:                      # If the user has correctly guessed the random number
+        print("Correct Guess!")
+        x = False
+    elif user_input > secret:                       # If the number guessed is higher than the random number
+        print("Guess is high!")
+        score -= 1
+    else:                                           # If the number guessed is lower than the random number
+        print("Guess is low")
+        score -= 1
+    if score < 1:                                   # If user runs out of tries
+        print("You ran out of tries😢")
+        x = False
+
+if score > 0:                                       # Printing the score if score is greater than 0
+    print("Your score is = ", score)
