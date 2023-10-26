0908b6c347917192b1121a62d64534b5cb1f0115
Gourab <gourabgaming111@gmail.com>
2023-10-26 22:18:20 +0530

    Modified while loop and printing of score

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,28 +1,23 @@
 import random
 secret = random.randint(1,10);
 
-x = True
 score = 10
+user_input = 0
 
 # Statement
 print("Guess a number between 1 and 10, you have 10 tries, the higher the score the better.")
 
-while x:
-    user_input = int(input("Enter a number = "))    # User Input
-    if ((user_input) < 1 or (user_input) > 10):     # If the guess is out of bounds
-        print("Guess out of bounds")
-    elif user_input == secret:                      # If the user has correctly guessed the random number
-        print("Correct Guess!")
-        x = False
-    elif user_input > secret:                       # If the number guessed is higher than the random number
-        print("Guess is high!")
+while user_input != secret:
+    user_input = int(input("Enter a number between 1 and 10: "))
+    if(user_input > secret):
+        print("Guess is too high")
         score -= 1
-    else:                                           # If the number guessed is lower than the random number
-        print("Guess is low")
+    elif(user_input < secret):
+        print("Guess is too low")
         score -= 1
-    if score < 1:                                   # If user runs out of tries
-        print("You ran out of tries😢")
-        x = False
+    if(score < 1):
+        print("Ran out of score")
+        break
 
-if score > 0:                                       # Printing the score if score is greater than 0
-    print("Your score is = ", score)
+if(score != 0):
+    print(f"You guessed the number {secret} with a score of {score}")
